70950c87041b05101d44292b3c02880d870249e4
turtwigle <127905136+turtwigle@users.noreply.github.com>
2023-05-25 16:25:37 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,32 @@
+let input1;
+let input2;
+let button;
+let banana1=("bananas");
+let apple2=("apples");
+let grape3=("grapes");
+let orange4=("oranges");
+
+button=document.querySelector(".yes");
+button.onclick = function() {
+    input1=document.querySelector(".this").value;
+    input2=document.querySelector(".that").value;
+    if(input1 <= 20 && input2 === "bananas"){
+        
+    }
+    if(input1 >=21 && input2 ==="apples"){
+        //red teletubby
+    }
+    if(input1 >=41 && input2 ==="grapes"){
+        //green teletubby
+    }
+    if (input1 >=61 && input2 ==="oranges"){
+        //purple teletubby
+    }
+    
+   
+    
+    
+    
+};
+
+
